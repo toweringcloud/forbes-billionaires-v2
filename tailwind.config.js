da986ba962e487c4bc095d4faf0fd2383f45d3f3
toweringcloud <toweringcloud@gmail.com>
2024-06-29 16:29:38 +0900

    layout head title up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      'abb-blue': '#3196FF',
-      'abb-gray': '#424242',
-      'abb-neutral': '#A3A3A3',
-      'abb-stone': '#FAFAFA',
-      'abb-white': '#FFFFFF'
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
